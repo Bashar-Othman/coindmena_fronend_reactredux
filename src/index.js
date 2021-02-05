ea5684a45ca20e80c6ba0353b20adda4959eb1c2
Bashar Othman <bashar.othman.message@gmail.com>
2021-02-05 07:18:51 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Main from './Screen/main';
+import EditPost from './Screen/EditPosts';
+import postReducer from './Reducer/postReducer'
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
+const store = createStore(postReducer)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Provider store={store}>
+        <BrowserRouter>
+            <Route exact path="/" component={Main} />
+            <Route path="/EditPost" component={EditPost} />
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById('root'));
+serviceWorker.unregister();
